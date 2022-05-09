c6adf3bce0ee9d2f10067fe614ea33eb4c1b3cdb
Mirbekov <@kylych-bob>
2022-05-09 20:50:21 +0600

    beginning_of_project

---
+++ aliens_invasion/alien_invasion.py
@@ -0,0 +1,53 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+
+class AlienInvasion:
+    """Класс для управления ресурсами и поведением игры."""
+    def __init__(self):
+        """Инициализирует игру и создает игровые ресурсы."""
+        pygame.init()
+        # Назначение цвета фона.
+        self.settings = Settings()
+        # self.bg_color = (230, 230, 230)
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
+        pygame.display.set_caption('Alien Invasion')
+        self.ship = Ship(self)
+
+    def run_game(self):
+        """Запуск основного цикла игры."""
+        while True:
+            self._check_events()
+            self._update_screen()
+
+    def _check_events(self):
+        """Обрабатывает нажатия клавиш и события мыши."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # Создание экземпляра и запуск игры.
+    ai = AlienInvasion()
+    ai.run_game()
+
+
+
+
+
+
+
+
+
+
