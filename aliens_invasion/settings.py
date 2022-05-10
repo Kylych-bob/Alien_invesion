a79763865032a93d5120d8cf303a6b12870ee3c0
Mirbekov <@kylych-bob>
2022-05-10 16:13:22 +0600

    Подробности смотри в файле readme.txt

--- aliens_invasion/settings.py
+++ aliens_invasion/settings.py
@@ -6,8 +6,20 @@ class Settings:
     def __init__(self):
         """Инициализирует настройки игры."""
         # Параметры экрана
-        self.screen_width = 1200
+        self.screen_width = 1300
         self.screen_height = 700
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (230, 230, 220)
+
+        # Параметры снаряда
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (170, 0, 0)
+        self.bullets_allowed = 3
+
+        # Скорость корабля
+        self.ship_speed = 1.5
+
+
 
 
